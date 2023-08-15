74ef240b4a4cec847305e188c4c8ce86f79a3f70
amogusussy <amogussussy@protonmail.com>
2023-08-15 18:24:03 +0100

    update dots

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,8 @@
 require "core"
 require "custom"
 
+vim.g["ff"] = "unix"
+
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
 if custom_init_path then
@@ -25,17 +27,59 @@ dofile(vim.g.base46_cache .. "defaults")
 local lspconfig = require('lspconfig')
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
+local on_attach = require("plugins.configs.lspconfig").on_attach
 
 lspconfig.emmet_ls.setup({
     -- on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue" },
+    filetypes = { "css", "eruby", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescriptreact", "vue", "htmldjango" },
     init_options = {
       html = {
         options = {
-          -- For possible options, see: https://github.com/emmetio/emmet/blob/master/src/config.ts#L79-L267
           ["bem.enabled"] = true,
         },
       },
     }
+})
+
+lspconfig.pylsp.setup({
+  -- on_attach = on_attach,
+  -- capabilities = capabilities,
+  filetypes = { "py", "python" },
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = { "E501" },
+        }
+      }
+    }
+  }
+})
+
+lspconfig.rust_analyzer.setup({
+  -- Server-specific settings. See `:help lspconfig-setup`
+  capabilities = capabilities,
+  on_attach = on_attach,
+  cmd = {
+    "rustup", "run", "stable", "rust-analyzer"
+  },
+  settings = {
+    ["rust-analyzer"] = {
+        imports = {
+            granularity = {
+                group = "module",
+            },
+            prefix = "self",
+        },
+        cargo = {
+            buildScripts = {
+                enable = true,
+            },
+        },
+        procMacro = {
+            enable = true
+        },
+    }
+}
 })
