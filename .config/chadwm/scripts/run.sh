b8e34ca8c205c40fd09fac23870e0a3e2575a14c
amogusussy <amogussussy@protonmail.com>
2023-09-03 21:44:31 +0100

    update dots

--- .config/chadwm/scripts/run.sh
+++ .config/chadwm/scripts/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-feh --no-fehbg --bg-fill '/home/matthew/Pictures/Wallpapers/mountain.jpg'
+feh --no-fehbg --bg-fill '/home/matthew/Pictures/Wallpapers/kurz.png'
 xset r rate 200 50 &
 export XDG_DATA_DIRS=$XDG_DATA_DIRS:~/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share
 slstatus &
