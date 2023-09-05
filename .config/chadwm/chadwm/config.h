c6fa265b0c34e9c2486f8b46a724d8fc3e2a23f5
amogusussy <amogussussy@protonmail.com>
2023-09-05 13:41:23 +0100

    update dots

--- .config/chadwm/chadwm/config.h
+++ .config/chadwm/chadwm/config.h
@@ -126,7 +126,7 @@ static const Layout layouts[] = {
     { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
     { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
-#define NOMOD 0
+#define NOMOD NULL
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
